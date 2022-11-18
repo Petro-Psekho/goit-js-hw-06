a84f8eaed4990e9bf4487f01f806e99040414299
Petro Psekho <psekho.pp@gmail.com>
2022-11-18 12:02:40 +0200

    adds task conditions

--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,25 @@
+// HTML містить порожній список ul#ingredients.
+
+// <ul id="ingredients"></ul>
+
+// JavaScript містить масив рядків.
+
+// const ingredients = [
+//   "Potatoes",
+//   "Mushrooms",
+//   "Garlic",
+//   "Tomatos",
+//   "Herbs",
+//   "Condiments",
+// ];
+
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+
+// 1) Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+// 2) Додасть назву інгредієнта як його текстовий вміст.
+// 3) Додасть елементу клас item.
+// 4) Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
