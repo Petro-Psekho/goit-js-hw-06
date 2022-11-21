e0ebb27f979794debbd18265c936e7778b46b18e
Petro Psekho <psekho.pp@gmail.com>
2022-11-21 16:24:43 +0200

    adds addEventListener on Button to task #4

--- js/task-04.js
+++ js/task-04.js
@@ -9,3 +9,23 @@
 // 1) Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
 // 2) Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
 // 3) Оновлюй інтерфейс новим значенням змінної counterValue.
+
+let counterValue = document.querySelector('#value');
+console.log(counterValue.textContent);
+
+counterValue = Number(counterValue.textContent);
+console.log(counterValue);
+
+const counterButtonRef = document.querySelectorAll('button');
+console.log(counterButtonRef[0].dataset.action);
+
+const counterButtonDecrementRef = counterButtonRef[0];
+console.log(counterButtonDecrementRef);
+
+const counterButtonIncrementRef = counterButtonRef[1];
+console.log(counterButtonDecrementRef);
+
+counterButtonDecrementRef.addEventListener('click', () => {
+  counterValue -= 1;
+  console.log(counterValue);
+});
