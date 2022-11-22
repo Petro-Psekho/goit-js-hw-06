847e1271bb10610bd01494b0d39fd27397135ea3
Petro Psekho <psekho.pp@gmail.com>
2022-11-22 20:24:38 +0200

    solves task #5

--- js/task-05.js
+++ js/task-05.js
@@ -3,45 +3,13 @@
 // <input type="text" id="name-input" placeholder="Please enter your name" />
 // <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
 
-//----------------------------------------------------------------------
-
-// const inputRef = document.querySelector('#name-input');
-// console.log(inputRef);
-
-// const nameLabelRef = document.querySelector('#name-output');
-// console.log(nameLabelRef);
-
-// inputRef.addEventListener('input', event => {
-//   console.log(event.currentTarget.value);
-//   nameLabelRef.textContent = event.currentTarget.value;
-// });
-
-//----------------------------------------------------------------------
-
 const inputRef = document.querySelector('#name-input');
-console.log(inputRef.attributes);
-inputRef.textContent = '';
-console.log(inputRef.textContent);
+console.log(inputRef);
 
 const nameLabelRef = document.querySelector('#name-output');
 console.log(nameLabelRef);
 
 inputRef.addEventListener('input', event => {
   console.log(event.currentTarget.value);
-  if ((inputRef.textContent = '')) {
-    nameLabelRef.innerHTML = 'Anonymous1111';
-  }
-
-  //   nameLabelRef.textContent = event.currentTarget.value;
-  //   inputRef.attributes.placeholder.value = event.currentTarget.value;
-
-  //   if ((inputRef.attributes.placeholder.value = 'Please enter your name')) {
-  //     nameLabelRef.textContent = event.currentTarget.value;
-  //     inputRef.attributes.placeholder.value = event.currentTarget.value;
-  //     console.log(inputRef.attributes.placeholder.value);
-  //   } else if ((inputRef.textContent = '')) {
-  //     nameLabelRef.innerHTML = 'Anonymous1111';
-  //   }
+  nameLabelRef.textContent = event.currentTarget.value;
 });
-
-//   nameLabelRef.innerHTML = 'Anony11111mous';
